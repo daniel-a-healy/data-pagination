657bf69a028377d654a9c9a108954c3eb80bd1fc
Daniel Healy <daniel.addison.healy@gmail.com>
2020-10-04 09:29:03 -0400

    Implemented showPage Function

--- js/script.js
+++ js/script.js
@@ -18,12 +18,44 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   let startIndex = (page * 9) - 9; 
+   let endIndex = page * 9;
+
+   let studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+
+   for(let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         let studentHTML =
+         `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+         `;
+
+         studentList.insertAdjacentHTML('afterend', studentHTML);
+      }
+   }
+}
+
+
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+
+function addPagination() {
+
+}
 
 
 
